41e10d187210bd53406ef01428d10c06435e1de0
pryan <pat.ryan@health.govt.nz>
2026-07-13 21:42:33 +0000

    fixed profile url

--- localscripts/fhirValidate.sh
+++ localscripts/fhirValidate.sh
@@ -7,12 +7,13 @@ cd ./output
 rm -r ./temp
 mkdir ./temp
 
-ls 
 echo validating CarePlan examples
 cp ../fsh-generated/resources/CarePlan*.json ./temp
 cp ../fsh-generated/resources/Bundle-CP*.json ./temp
-
-java -jar ../validator_cli.jar ./temp  -ig ./package.tgz -profile http://hl7.org.nz/fhir/StructureDefinition/BsaCarePlan -proxy WebProxy-80fef376c00ea74f.elb.ap-southeast-2.amazonaws.com:3128 -html-output ./validation_CarePlan.html
+ls ./package.tgz
+java -jar ../validator_cli.jar ./temp  -ig ./package.tgz -profile http://hl7.org.nz/fhir/ig/bsa/StructureDefinition/BsaCarePlan -proxy WebProxy-80fef376c00ea74f.elb.ap-southeast-2.amazonaws.com:3128 -html-output ./validation_CarePlan.html
 rm ./*temp/*.json
 
 cd ..
+
+
