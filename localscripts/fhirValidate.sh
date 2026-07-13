a6ada7961cfdd07dd51c11c42d91a49a51bd7e10
pryan <pat.ryan@health.govt.nz>
2026-07-13 22:19:15 +0000

    mv validator

--- localscripts/fhirValidate.sh
+++ localscripts/fhirValidate.sh
@@ -11,7 +11,7 @@ echo validating CarePlan examples
 cp ../fsh-generated/resources/CarePlan*.json ./temp
 cp ../fsh-generated/resources/Bundle-CP*.json ./temp
 ls ./package.tgz
-java -jar ../validator_cli.jar ./temp  -ig ./package.tgz -profile http://hl7.org.nz/fhir/ig/bsa/StructureDefinition/BsaCarePlan -proxy WebProxy-80fef376c00ea74f.elb.ap-southeast-2.amazonaws.com:3128 -html-output ./validation_CarePlan.html
+java -jar ~/validator_cli.jar ./temp  -ig ./package.tgz -profile http://hl7.org.nz/fhir/ig/bsa/StructureDefinition/BsaCarePlan -proxy WebProxy-80fef376c00ea74f.elb.ap-southeast-2.amazonaws.com:3128 -html-output ./validation_CarePlan.html
 rm ./*temp/*.json
 
 cd ..
